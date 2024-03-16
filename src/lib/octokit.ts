30c06c2b90c47d122ec28162b32531bf95b365eb
ajtollo <khesir2002@gmail.com>
2024-03-17 03:28:25 +0800

    Environment variable fix

--- src/lib/octokit.ts
+++ src/lib/octokit.ts
@@ -1,15 +1,20 @@
 import { Octokit } from "octokit";
 
+
+const githubToken = process.env.Token;
+
+
 export default class OctokitSingleton {
   private static instance: Octokit;
 
   private constructor() {} // Prevent instantiation outside the class
 
   public static getInstance(): Octokit {
+
     if (!OctokitSingleton.instance) {
       // Create a new instance if it doesn't exist
       OctokitSingleton.instance = new Octokit({
-        auth: process.env.Token
+        auth: githubToken
       });
     }
     return OctokitSingleton.instance;
