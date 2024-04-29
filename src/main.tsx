ea6a51350b548e0b63798a42e329acd225ef65ff
ajtollo <khesir2002@gmail.com>
2024-04-29 23:55:03 +0800

    app.ts added

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import * as appActions from "./actions/app.ts"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -10,3 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
+appActions.register()
