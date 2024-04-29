d03b900b1f922903f890074564b394802322b1ff
ajtollo <khesir2002@gmail.com>
2024-04-29 21:36:48 +0800

    add pwa function

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ 
+    react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -15,5 +15,6 @@ export default defineConfig({
   },
   define: {
     'process.env.VITE_TOKEN' : JSON.stringify(process.env.VITE_TOKEN)
-  }
+  },
+  
 })
