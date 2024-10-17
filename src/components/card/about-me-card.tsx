10c5f1e516052609c32a94da67d3a611e9bfa81f
khesir <khesir2002@gmail.com>
2024-10-17 15:06:06 +0800

    feat: fix mobile responsiveness

--- src/components/card/about-me-card.tsx
+++ src/components/card/about-me-card.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export function AboutMeCard() {
   return (
     <motion.div
-        className="absolute w-1/3 p-5 bottom-5 -right-5 bg-white z-20 rounded-3xl 
+        className="absolute w-2/3 md:w-1/3 p-5 bottom-5 -right-5 bg-white z-20 rounded-3xl 
         custom-shadow overflow-hidden dark:bg-slate-700 dark:border-gray-700 dark:text-white"
         initial={{ x: "100%", opacity: 0 }} // Start off-screen to the right and invisible
         animate={{ x: 0, opacity: 1 }} // Animate to the original position and fully visible
